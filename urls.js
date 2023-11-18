c9235e343aab66f1ba626fa44beef10577f502e5
Bert Spaan <bert.spaan@gmail.com>
2023-11-18 17:23:28 +0100

    new urls

--- urls.js
+++ urls.js
@@ -1,1 +1,11 @@
-const urls = ["https://tikkie.me/pay/ak1qk0oo0g2bc8873bel"];
+const urls = [
+  "https://tikkie.me/pay/ak1qk0oo0g2bc8873bel",
+  "https://tikkie.me/pay/f5auvau0ika01en7j5u9",
+  "https://tikkie.me/pay/pgtjj8i7bituqmgl2img",
+  "https://tikkie.me/pay/v2d62ticf3vrlbthejpk",
+  "https://tikkie.me/pay/7ro3vkeh08uimk9338h7",
+  "https://tikkie.me/pay/c2dstvao9ts9nlidkv23",
+  "https://tikkie.me/pay/d8rbv4ehmqhuhtc0u1p9",
+  "https://tikkie.me/pay/jr24fcsgrimmiv69j0e5",
+  "https://tikkie.me/pay/pe5jbtn1r63fdgggi4i9",
+];
